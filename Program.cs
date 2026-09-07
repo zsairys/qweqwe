485a81ea72b9107f02657ba94aab0e5ddb713a08
zsairys <sairys06@vk.com>
2026-09-07 19:15:34 +0700

    исправил ошибку в readme

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace pis1
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            List<Value> list = new List<Value>();
+            Console.WriteLine("Ввести название валют (2 строки), курс (дробное число), дата. для выхода введите пустую строку");
 
+            while (true)
+            {
+                string s = Console.ReadLine();
+                if (s == "") { break; }
+
+                string[] prop = s.Split(',');
+                list.Add(new Value
+                {
+                    Name = prop[0],
+                    NameRu = prop[1],
+                    Course = Convert.ToDouble(prop[2]),
+                    Date = Convert.ToDateTime(prop[3])
+                });
+            }
         }
+    }
 }
--- value.cs
+++ value.cs
@@ -6,15 +6,11 @@ using System.Threading.Tasks;
 
 namespace pis1
 {
-    internal class value
+    class Value
     {
-        private string name1;
-        private string name2;
-        private float course;
-        public string Name { get { return name1; } set { name1 = value; } }
-        public string NameRu { get { return name2; } set { name2 = value; } }
-        public DateTime date;
-        public float Course { get { return course; } set { course = value; } }
-        public DateTime Date { get { return date; } set { date = value; } }
+        public string Name { get; set; }
+        public string NameRu { get; set; }
+        public double Course { get; set; }
+        public DateTime Date { get; set; }
     }
 }
