d3f90ab03d82a1645f3f0fce41e56ec224d3452a
zsairys <sairys06@vk.com>
2026-09-07 18:41:14 +0700

    1 laba

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace pis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+}
---
+++ value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class value
+    {
+        private string name1;
+        private string name2;
+        private float course;
+        public string Name { get { return name1; } set { name1 = value; } }
+        public string NameRu { get { return name2; } set { name2 = value; } }
+        public DateTime date;
+        public float Course { get { return course; } set { course = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+    }
+}
